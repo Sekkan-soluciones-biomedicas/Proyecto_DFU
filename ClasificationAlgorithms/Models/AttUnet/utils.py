acb94e68d871abfdd76c94edffc60664cdf73066
Adrian Maldonado <am96929@gmal.com>
2025-02-23 18:30:47 -0600

    chore: Change name to dice_loss graph for attunet in ClasAlg. Además quita el sufijo semisup_ft al momento de guardar el modelo.

--- ClasificationAlgorithms/Models/AttUnet/utils.py
+++ ClasificationAlgorithms/Models/AttUnet/utils.py
@@ -133,7 +133,7 @@ def plot_dice_loss(L_dice_result, L_loss_result, show_plot=False):
     plt.title('Curvas de Dice Score y Loss Function')
 
     # Guardado de la gráfica:
-    plt.savefig("output_assets_model/dice_loss_graph.png")
+    plt.savefig("output_assets_model/dice_loss_graph_AttentionUnet.png")
     print(f"Gráfica de Loss y Dice guardada en: output_assets_model/dice_loss_graph_AttentionUnet.png")
     # Mostrar la gráfica (opcional):
     if show_plot:
