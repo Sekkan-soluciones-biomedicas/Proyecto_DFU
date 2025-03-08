36c962e19b3ef1ed2746631e48298af015a121ce
Adrian Maldonado <am96929@gmal.com>
2025-03-08 04:20:46 -0600

    fix: Agrega la función de sum of losses al script de optuna_train.py y además corrige los rangos de los hypp de acuerdo a lo recomendado por grok

--- ClasificationAlgorithms/Models/ResUnet/optuna_train.py
+++ ClasificationAlgorithms/Models/ResUnet/optuna_train.py
@@ -13,7 +13,7 @@ import numpy as np
 import torch.optim as optim
 import optuna
 from main import ResUnet
-from metrics import check_metrics, dice_loss_multiclass
+from metrics import check_metrics, dice_loss_multiclass, sum_of_losses
 from utils import get_loaders
 
 # --------------- history --------------------------
@@ -45,10 +45,14 @@ print("Device: ", DEVICE, "is available \n ----------------------")
 # TRAIN_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded/train_masks"
 # VAL_IMG_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded/val_images"
 # VAL_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded/val_masks"
-TRAIN_IMG_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/train/images"
-TRAIN_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/train/masks"
-VAL_IMG_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/val/images"
-VAL_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/val/masks"
+# TRAIN_IMG_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/train/images"
+# TRAIN_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/train/masks"
+# VAL_IMG_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/val/images"
+# VAL_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_strat_for_training/val/masks"
+TRAIN_IMG_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_stratified/train_b/images"
+TRAIN_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_stratified/train_b/masks"
+VAL_IMG_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_stratified/val/images"
+VAL_MASK_DIR = "C:/Users/am969/Documents/DFU_Proyect/ClasificationAlgorithms/data_TissueSegNet/data_padded_stratified/val/masks"
 
 # epochs_per_trial = 12
 # n_trials = 50
@@ -104,67 +108,59 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
 
 def objective(trial):
     # Hyperparameter optimization
-    learning_rate = trial.suggest_loguniform('learning_rate', 1e-6, 1e-2)
+    learning_rate = trial.suggest_loguniform('learning_rate', 1e-6, 1e-3)
     optimizer_name = trial.suggest_categorical('optimizer', ['Adam', 'AdamW'])
     batch_size = trial.suggest_int('batch_size', 2, 8)
-    dropout_prob = trial.suggest_uniform('dropout_prob', 0.0, 0.5)
+    dropout_prob = trial.suggest_uniform('dropout_prob', 0.0, 0.4)
     weight_decay = trial.suggest_loguniform("weight_decay", 1e-6, 1e-3) if optimizer_name == "AdamW" else None
 
     model = ResUnet(in_channels=3, out_channels=4, dropout=dropout_prob).to(DEVICE)
-    loss_fn = dice_loss_multiclass
+    # loss_fn = dice_loss_multiclass
+    loss_fn = sum_of_losses
     optimizer = optim.Adam(model.parameters(), lr=learning_rate) if optimizer_name == 'Adam' else optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-    # train_transform = A.Compose([
-    #     A.Resize(height=240, width=240),
-    #     A.Rotate(limit=35, p=0.5),
-    #     A.HorizontalFlip(p=0.5),
-    #     A.VerticalFlip(p=0.1),
-    #     A.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0], max_pixel_value=255.0),
-    #     ToTensorV2(),
-    # ])
     train_transform = A.Compose([
         A.Resize(height=240, width=240),
         A.Rotate(limit=35, p=0.5),
 
+        A.OneOf(
+            [
+                A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0, p=0.1, border_mode=0),
+                A.ShiftScaleRotate(scale_limit=0, rotate_limit=30, shift_limit=0, p=0.1, border_mode=0),
+                A.ShiftScaleRotate(scale_limit=0.3, rotate_limit=0, shift_limit=0.1, p=0.6, border_mode=0),
+                A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=30, shift_limit=0.1, p=0.2, border_mode=0),
+            ],
+            p=0.9,
+        ),
 
-         A.OneOf(
-                [
-                    A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0, p=0.1, border_mode=0),
-                    A.ShiftScaleRotate(scale_limit=0, rotate_limit=30, shift_limit=0, p=0.1, border_mode=0),
-                    A.ShiftScaleRotate(scale_limit=0, rotate_limit=0, shift_limit=0.1, p=0.6, border_mode=0),
-                    A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=30, shift_limit=0.1, p=0.2, border_mode=0),
-                ],
-                p=0.9,
-            ),
+        A.OneOf(
+            [
+                A.Perspective(p=0.2),
+                A.GaussNoise(p=0.2),
+                A.Sharpen(p=0.2),
+                A.Blur(blur_limit=3, p=0.2),
+                A.MotionBlur(blur_limit=3, p=0.2),
+            ],
+            p=0.6,
+        ),
 
-            A.OneOf(
-                [
-                    A.Perspective(p=0.2),
-                    A.GaussNoise(p=0.2),
-                    A.Sharpen(p=0.2),
-                    A.Blur(blur_limit=3, p=0.2),
-                    A.MotionBlur(blur_limit=3, p=0.2),
-                ],
-                p=0.6,
-            ),
+        A.OneOf(
+            [
+                A.HorizontalFlip(p=0.5),
+                A.VerticalFlip(p=0.5),
+            ],
+            p=0.8,
+        ),
 
-            A.OneOf(
-                [
-                    A.HorizontalFlip(p=0.5),
-                    A.VerticalFlip(p=0.5),
-                ],
-                p=0.8,
-            ),
-
-            A.OneOf(
-                [
-                    A.CLAHE(p=0.25),
-                    A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.25),
-                    A.RandomGamma(p=0.25),
-                    A.HueSaturationValue(p=0.25),
-                ],
-                p=0.3,
-            ),
+        A.OneOf(
+            [
+                A.CLAHE(p=0.25),
+                A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.25),
+                A.RandomGamma(p=0.25),
+                A.HueSaturationValue(p=0.25),
+            ],
+            p=0.3,
+        ),
 
         A.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0], max_pixel_value=255.0),
         ToTensorV2(),
